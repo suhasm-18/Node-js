73272dde6617c8e3dafb85f29c3cc925c0a2a3f4
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-09-01 10:33:24 +0530

    Create osmodule.js

---
+++ OS-module/osmodule.js
@@ -0,0 +1,8 @@
+const os = require('os');
+                                        //output
+console.log(os.freemem());              // 657137664
+console.log(os.homedir());              // C:\Users\suhas.m
+console.log(os.hostname());            // LAPTOP-3EJI9BCS
+console.log(os.release());             // 10.0.22621
+
+
